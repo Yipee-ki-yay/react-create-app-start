b01e3de7086ce96f9df8a58ca4f08f7ce6ce9da4
Yipee-ki-yay <mopoz07@gmail.com>
2018-01-21 20:58:18 +0200

    add ToDo app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import TodosList from './todos';
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
   render() {
     return (
       <div className="App">
@@ -11,15 +21,57 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To Do
         </p>
-        <section className="App-main">
 
+        <section className="App-main">
+          <form onSubmit={this.handleSubmit}>
+            <input
+              onChange={this.handleChange}
+              value={this.state.text}
+            />
+            <button>save</button>
+            {/* <p>{this.state.todos[0]}<p/> */}
+            <TodoList items={this.state.items} />
+          </form>
         </section>
+
+
         <footer className="App-footer">
 
         </footer>
       </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(prevState => ({
+      items: prevState.items.concat(newItem),
+      text: ''
+    }));
+  }
+}
+
+class TodoList extends React.Component {
+  render() {
+    return (
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text}</li>
+        ))}
+      </ul>
     );
   }
 }
